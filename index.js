7f0ee1a2e23ff19a3ce9be6b44d7a5d69552c609
JC Camargo <jccamargo15@gmail.com>
2019-05-02 00:35:40 -0300

    desafio

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.set('view engine', 'njk')
 
 const users = ['Diego Margues', 'Robson Fernandes', 'Cleiton Souza']
 
+const checkAge = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/age')
+  }
+
+  return next()
+}
+
 app.get('/', (req, res) => {
   return res.render('list', { users })
 })
@@ -25,6 +35,31 @@ app.get('/new', (req, res) => {
 app.post('/create', (req, res) => {
   users.push(req.body.user)
   return res.redirect('/')
+})
+
+app.get('/age', (req, res) => {
+  return res.render('age')
+})
+
+app.get('/major', checkAge, (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', checkAge, (req, res) => {
+  const { age } = req.query
+
+  return res.render('minor', { age })
+})
+
+app.post('/check', (req, res) => {
+  if (req.body.age >= 18) {
+    // res.send(req.body.age)
+    return res.redirect(`/major?age=${req.body.age}`)
+  } else {
+    return res.redirect(`/minor?age=${req.body.age}`)
+  }
 })
 
 app.listen(3000)
